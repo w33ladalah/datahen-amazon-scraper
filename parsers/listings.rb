3657dc7cbbbdcb8280bb5f26388b2b987e503a8f
Hendro <hendrothemail@gmail.com>
2022-03-04 21:43:48 +0700

    Fix listings parser and add products parser

--- parsers/listings.rb
+++ parsers/listings.rb
@@ -2,13 +2,16 @@ nokogiri = Nokogiri.HTML(content)
 products = nokogiri.css('li.octopus-pc-item-v3')
 
 products.each do |product|
-  href = product.at_css('a')['href']
+  href = product.at_css('a')['href'].split('?').first
   url = URI.join('https://www.amazon.com', href).to_s
   display_width = rand 800..1920
   display_height = rand 728..1280
   pages << {
 		url: url,
 		page_type: 'products',
-		vars: {}
+		vars: {
+			category: page['vars']['category'],
+            url: url
+		}
     }
 end
--- parsers/products.rb
+++ parsers/products.rb
@@ -3,40 +3,62 @@ nokogiri = Nokogiri.HTML(content)
 # initialize an empty hash
 product = {}
 
+#save the url
+product['url'] = page['vars']['url']
+
+#save the category
+product['category'] = page['vars']['category']
+
+#extract the asin
+canonical_link = nokogiri.css('link').find{|link| link['rel'].strip == 'canonical' }
+product['asin'] = canonical_link['href'].split("/").last
+
 #extract title
-product['title'] = nokogiri.at_css('h1.dark-gray').text.strip
+product['title'] = nokogiri.at_css('#productTitle').text.strip
 
-#extract current price
-product['current_price'] = nokogiri.at_css('div[data-testid="add-to-cart-price-atf"] span[itemprop="price"]').text.strip
-
-#extract original price
-original_price_div = nokogiri.at_css('div[data-testid=add-to-cart-price-atf] .nowrap span.strike')
-original_price = original_price_div ? original_price_div.text.strip.gsub('$','').to_f : nil
-product['original_price'] = original_price == 0.0 ? nil : original_price
-
-#extract rating
-rating = nokogiri.at_css('span.rating-number').text.strip.gsub(/[)(]/, '').to_f
-product['rating'] = rating == 0 ? nil : rating
+#extract seller/author
+seller_node = nokogiri.at_css('a#bylineInfo')
+if seller_node
+  product['seller'] = seller_node.text.strip
+else
+  product['author'] = nokogiri.css('a.contributorNameID').text.strip
+end
 
 #extract number of reviews
-review_text = nokogiri.at_css('a[link-identifier="reviewsLink"]').text.strip
-product['reviews_count'] = review_text =~ /reviews/ ? review_text.split(' ').first.to_i : 0
+reviews_node = nokogiri.at_css('span#acrCustomerReviewText')
+reviews_count = reviews_node ? reviews_node.text.strip.split(' ').first.gsub(',','') : nil
+product['reviews_count'] = reviews_count =~ /^[0-9]*$/ ? reviews_count.to_i : 0
 
-#extract publisher
-product['publisher'] = nokogiri.at_css('div[itemprop="brand"]').text.strip
+#extract rating
+rating_node = nokogiri.at_css('#averageCustomerReviews span.a-icon-alt')
+stars_num = rating_node ? rating_node.text.strip.split(' ').first : nil
+product['rating'] = stars_num =~ /^[0-9.]*$/ ? stars_num.to_f : nil
 
-#extract walmart item number
-canonical_url = nokogiri.at_css('link[rel="canonical"]')['href']
-product['walmart_number'] = canonical_url.split('/').last.strip
+#extract price
+price_node = nokogiri.at_css('#price_inside_buybox', '#priceblock_ourprice', '#priceblock_dealprice', '.offer-price')
+if price_node
+  product['price'] = price_node.text.strip.gsub(/[\$,]/,'').to_f
+end
 
-#extract product image
-product['img_url'] = nokogiri.at_css('.relative img.db')['src'].split('?').first
+#extract availability
+availability_node = nokogiri.at_css('#availability')
+if availability_node
+  product['available'] = availability_node.text.strip == 'In Stock.' ? true : false
+else
+  product['available'] = nil
+end
 
-#extract product categories
-product['categories'] = nokogiri.css('nav[aria-label="breadcrumb"] li').collect{|li| li.text.strip.gsub('/','') }
+#extract product description
+description = ''
+nokogiri.css('#feature-bullets li').each do |li|
+  unless li['id'] || (li['class'] && li['class'] != 'showHiddenFeatureBullets')
+    description += li.text.strip + ' '
+  end
+end
+product['description'] = description.strip
 
 # specify the collection where this record will be stored
-product['_collection'] = 'products'
+product['_collection'] = "products"
 
 # save the product to the job’s outputs
 outputs << product
