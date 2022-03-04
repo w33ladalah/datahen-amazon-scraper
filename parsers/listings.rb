87da4a3c16eb3a1ad88b01999b5a624a67bbb090
Hendro <hendrothemail@gmail.com>
2022-03-04 21:32:11 +0700

    Fix listings parser

--- parsers/listings.rb
+++ parsers/listings.rb
@@ -3,7 +3,7 @@ products = nokogiri.css('li.octopus-pc-item-v3')
 
 products.each do |product|
   href = product.at_css('a')['href']
-  url = URI.join('https://www.walmart.com', href).to_s
+  url = URI.join('https://www.amazon.com', href).to_s
   display_width = rand 800..1920
   display_height = rand 728..1280
   pages << {
