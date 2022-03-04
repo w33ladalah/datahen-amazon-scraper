7552374c48ae27e3fa1081dbc3f85ce2d90f1ed4
Hendro <hendrothemail@gmail.com>
2022-03-04 21:01:17 +0700

    Add parser to config

--- parsers/listings.rb
+++ parsers/listings.rb
@@ -1,4 +1,4 @@
-products = nokogiri.css('ol[role="list"] li', 'div[data-card-metrics-id="octopus-search-result-card_apb-browse_0"] a')
+products = nokogiri.css('li.octopus-pc-item-v3')
 
 products.each do |product|
   href = product.at_css('a')['href']
@@ -8,18 +8,6 @@ products.each do |product|
   pages << {
 		url: url,
 		page_type: 'products',
-		fetch_type: 'browser',
-		headers: {
-			"User-Agent" => "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.102 Safari/537.36",
-			"sec-ch-ua" => '" Not A;Brand";v="99", "Chromium";v="98", "Google Chrome";v="98"',
-			"sec-ch-ua-mobile" => "?0",
-			"sec-ch-ua-platform" => "Linux"
-		},
-		display: {
-			"width": display_width,
-			"height": display_height
-		},
-		no_default_headers: true,
 		vars: {}
     }
 end
